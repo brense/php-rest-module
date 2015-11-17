88d7cbefc7b9298c1b6815c6ff4526ec3b419ec2
brense <b.rense@gmail.com>
2015-11-17 21:38:50 +0100

    added missing get/set/isset parameters

--- iResourceModel.php
+++ iResourceModel.php
@@ -6,9 +6,9 @@ interface iResourceModel {
 
     public function toArray();
 
-    public function __get();
+    public function __get($property);
 
-    public function __set();
+    public function __set($property, $value);
     
-    public function __isset();
+    public function __isset($property);
 }
